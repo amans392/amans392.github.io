57b71db4afeebdb203ae2335bda42f85626cb1d3
amans392 <71714627+amans392@users.noreply.github.com>
2025-03-08 19:35:27 -0500

    Updated Portfolio Website
    Made a series of updates to my portfolio website to make it more responsive.  Added updated sections on skills I've built up in  responsive design, layout and added sections on my projects involving cyber security

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".autotype", {
+
+    strings:["IT Professional" , "Web Developer", "Baking Enthusiast"],
+    typespeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+})
+
